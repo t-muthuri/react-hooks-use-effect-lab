6808861c1d6ab5e32feb5cde1b1843ad4826aac7
Tressie Muthuri <tressiemuthuri@gmail.com>
2022-10-04 13:07:16 +0300

    Completed assignment

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,10 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    const timer = setTimeout(()=> {
+      setTimeRemaining((timeRemaining) => timeRemaining -1);
+    }, 1000);
+    //   setCount('Timeout called!');
+    // }, 1000);
+    // effect
+    return () => {
+      // cleanup
+      clearTimeout(timer);
+    };
+  }, [timeRemaining, onAnswered]);
+  //the component is re-rendered every time the two variables are called
 
+  if (timeRemaining === 0){
+    setTimeRemaining(10); 
+    //re-adjusts the time and prevents the timer from becoming a negative value
+    onAnswered(false);
+    return; 
+  }
+  
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
